b25338161055b148521fdbef37a52e9263905fa4
sample1927 <55770397+sample1927@users.noreply.github.com>
2019-10-01 11:23:45 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,13 @@ int main(int argc, char *argv[])
 {
     char str[5];
     
+    str[0] = 5;
+    str[1] = -2;
+    str[2] = 4;
+    str[3] = -5;
+    str[4] = 7;
+    str[5] = -4
+    
     while(fgets(str, sizeof(str), stdin)){
         printf("%s", str);
     }
