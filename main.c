f26f269e968f6e062dca644f67d83b1bf49140d5
sample1927 <55770397+sample1927@users.noreply.github.com>
2019-10-05 17:49:33 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ int main(int argc, char *argv[])
 {
     char str[5];
     
+    printf("数字を入力してください。\n");
+    scanf_s("%5s", str, 4);
     while(fgets(str, sizeof(str), stdin)){
         printf("%s", str);
     }
